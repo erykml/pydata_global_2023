ff66cbb713b867d639fe1a0a6b34a3b227ab1f0a
Eryk Lewinson <elewinson@bol.com>
2023-12-03 15:49:05 +0100

    added the eval stage

---
+++ src/evaluate.py
@@ -0,0 +1,40 @@
+import json
+import os
+from pathlib import Path
+
+import joblib
+import pandas as pd
+from dvclive.lgbm import DVCLiveCallback
+from joblib import dump
+from lightgbm import LGBMClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+from constants import DATA_PROCESSED_DIR, MODELS_DIR
+from dvclive import Live
+from utils import load_params
+
+# Load the model
+model = joblib.load(f"{MODELS_DIR}/model.joblib")
+
+# load test data
+X_test = pd.read_csv(f"{DATA_PROCESSED_DIR}/X_test.csv", index_col="Name")
+y_test = pd.read_csv(f"{DATA_PROCESSED_DIR}/y_test.csv", index_col="Name")
+
+# obtain predictions
+y_pred = model.predict(X_test)
+y_pred_prob = model.predict_proba(X_test)[:, 1]
+y_test_array = y_test.values.ravel()
+
+# evaluate - get metrics + plots
+live = Live("eval_plots")
+live.log_sklearn_plot("confusion_matrix", y_test_array, y_pred)
+live.log_sklearn_plot("roc", y_test_array, y_pred_prob)
+live.log_sklearn_plot("precision_recall", y_test_array, y_pred_prob)
+
+metrics = {
+    "accuracy": round(accuracy_score(y_test, y_pred), 4),
+    "recall": round(recall_score(y_test, y_pred), 4),
+    "precision": round(precision_score(y_test, y_pred), 4),
+}
+
+json.dump(obj=metrics, fp=open("metrics.json", "w"), indent=4, sort_keys=True)
--- src/train.py
+++ src/train.py
@@ -1,38 +1,37 @@
+import json
 import os
 from pathlib import Path
-import json
 
 import pandas as pd
+from dvclive.lgbm import DVCLiveCallback
 from joblib import dump
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 from lightgbm import LGBMClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 from constants import DATA_PROCESSED_DIR, MODELS_DIR
-from utils import load_params
-
 from dvclive import Live
-from dvclive.lgbm import DVCLiveCallback
+from utils import load_params
 
 params = load_params()["train"]
 
 # load training data
 X_train = pd.read_csv(f"{DATA_PROCESSED_DIR}/X_train.csv", index_col="Name")
 y_train = pd.read_csv(f"{DATA_PROCESSED_DIR}/y_train.csv", index_col="Name")
-X_test = pd.read_csv(f"{DATA_PROCESSED_DIR}/X_test.csv", index_col="Name")
-y_test = pd.read_csv(f"{DATA_PROCESSED_DIR}/y_test.csv", index_col="Name")
 
 # train a model
-# model = RandomForestClassifier(random_state=42, **params["params"])
 model = LGBMClassifier(random_state=42, objective="binary", **params["params"])
 
 with Live(save_dvc_exp=True) as live:
+    print("Training the model ----")
+
     # for simplicity, we use the training set as validation set
     fit_params = {
-        "eval_set": [(X_test, y_test)],
+        "eval_set": [(X_train, y_train)],
         "eval_names": ["valid"],
         "callbacks": [DVCLiveCallback(live=live)],
     }
     model.fit(X_train, y_train, **fit_params)
+    print("Training completed. Storing the model...")
 
     # store the trained model
     model_dir = Path(MODELS_DIR)
@@ -40,20 +39,4 @@ with Live(save_dvc_exp=True) as live:
 
     dump(model, f"{MODELS_DIR}/model.joblib")
 
-    # obtain predictions
-    y_pred = model.predict(X_test)
-    y_pred_prob = model.predict_proba(X_test)[:, 1]
-    y_test_array = y_test.values.ravel()
-
-    # evaluate - get metrics + plots
-    live.log_sklearn_plot("confusion_matrix", y_test_array, y_pred)
-    live.log_sklearn_plot("roc", y_test_array, y_pred_prob)
-    live.log_sklearn_plot("precision_recall", y_test_array, y_pred_prob)
-
-    metrics = {
-        "accuracy": round(accuracy_score(y_test, y_pred), 4),
-        "recall": round(recall_score(y_test, y_pred), 4),
-        "precision": round(precision_score(y_test, y_pred), 4),
-    }
-
-    json.dump(obj=metrics, fp=open("metrics.json", "w"), indent=4, sort_keys=True)
+    print("Done!")
