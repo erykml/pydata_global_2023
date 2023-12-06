95a4734fb4fca0a1973ce484a48c483d3d53c50f
Eryk Lewinson <elewinson@bol.com>
2023-12-06 01:36:34 +0100

    adjusted gs

--- src/simple_grid_search.py
+++ src/simple_grid_search.py
@@ -3,13 +3,10 @@ import subprocess
 import itertools
 
 # param values
-n_estimators_grid = [10, 100]
-max_depth_grid = [5, "null"]
-class_weight_grid = ["balanced", "null"]
+n_estimators_grid = [10, 20]
+max_depth_grid = [5, 10]
 
-for n_est, max_depth, class_weight in itertools.product(
-    n_estimators_grid, max_depth_grid, class_weight_grid
-):
+for n_est, max_depth in itertools.product(n_estimators_grid, max_depth_grid):
     subprocess.run(
         [
             "dvc",
@@ -20,7 +17,5 @@ for n_est, max_depth, class_weight in itertools.product(
             f"train.params.n_estimators={n_est}",
             "--set-param",
             f"train.params.max_depth={max_depth}",
-            "--set-param",
-            f"train.params.class_weight={class_weight}",
         ]
     )
